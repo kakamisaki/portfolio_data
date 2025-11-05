40108fe985e4793f2ed7a089b9759439c986273b
kakamisaki <33ki.kane46@gmail.com>
2025-11-06 00:25:02 +0400

    portfolio_2
    変更部分
    ・写真の書き出し
    ・犬の足跡（幅の調整ができていません）
    ・各テキストのフォント
    ・アバウトページの丸型写真

--- portfolio＿Kaneshiro/script.js
+++ portfolio＿Kaneshiro/script.js
@@ -1,90 +1,123 @@
-const toggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.menu-nav');
+// ====================
+// ハンバーガーメニュー
+// ====================
+const toggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".menu-nav");
 
-toggle.addEventListener('click', () => {
-  nav.classList.toggle('active');
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
   // メニューが開いたら「✕」、閉じたら「☰」に切り替え
-  if (nav.classList.contains('active')) {
-    toggle.textContent = '✕';
-    toggle.setAttribute('aria-label', 'メニューを閉じる');
+  if (nav.classList.contains("active")) {
+    toggle.textContent = "✕";
+    toggle.setAttribute("aria-label", "メニューを閉じる");
   } else {
-    toggle.textContent = '☰';
-    toggle.setAttribute('aria-label', 'メニューを開く');
+    toggle.textContent = "☰";
+    toggle.setAttribute("aria-label", "メニューを開く");
   }
 });
 
-// HTMLの要素を取得する部分、モーダルを操作するための
+// ====================
+// スライダー
+// ====================
+$(function () {
+  $(".main-visual__map").slick({
+    autoplay: true, // 自動でスクロール
+    autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
+    speed: 15000, // スライドが流れる速度を設定
+    cssEase: "linear", // スライドの流れ方を等速に設定
+    slidesToShow: 1, // 表示するスライドの数
+    swipe: false, // 操作による切り替えはさせない
+    arrows: false, // 矢印非表示
+    pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
+    pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
+  });
+});
+
+
+$(function () {
+  $(".slides").slick({
+    prevArrow: $(".ctrl .prev"),
+    nextArrow: $(".ctrl .next"),
+  });
+});
+
 const modal = document.getElementById("modal");
 const gallery = document.getElementById("modal-gallery");
-const closeBtn = document.querySelector(".close");
+const closeBtn = document.querySelector(".close-btn");
 
-// モーダルを「表示させる」ための
+
+
+
+// // モーダルを「表示させる」ための
 const showImages = (images) => {
-  gallery.innerHTML = images.map(src => `<img src="${src}" alt="">`).join('');
-  modal.style.display = "block";
-};
-
+gallery.innerHTML = images.map((src) => `<img src="${src}" alt="">`).join("");
+modal.style.display = "block";
+ };
 
 document.querySelector(".btn-africa").addEventListener("click", () => {
   showImages([
-    "./imaages/af_1.jpg",
-    "./imaages/af_2.jpg",
-    "./imaages/af_3.jpg",
-    "./imaages/af_4.jpg",
-    "./imaages/af_5.jpg",
-    "./imaages/af_6.jpg",
-    "./imaages/af_7.jpg",
-    "./imaages/af_8.jpg",
-    "./imaages/af_9.jpg"
+    "./photo/af_1.jpg",
+    "./photo/af_2.jpg",
+    "./photo/af_3.jpg",
+    "./photo/af_4.jpg",
+    "./photo/af_5.jpg",
   ]);
 });
 
 
 document.querySelector(".btn-canada").addEventListener("click", () => {
-  showImages([
-    "./imaages/ca_1.jpg",
-    "./imaages/ca_3.jpg",
-    "./imaages/ca_4.jpg"
-  ]);
+showImages([
+  "./photo/ca__1.jpg",
+    "./photo/ca__2.jpg",
+    "./photo/ca__3.jpg",
+    "./photo/ca__4.jpg",
+ ]);
 });
-
 
 document.querySelector(".btn-europe").addEventListener("click", () => {
-  showImages([
-    "./imaages/eu_2.jpg",
-    "./imaages/eu_3.jpg",
-    "./imaages/eu_4.jpg"
-    
-  ]);
+showImages([
+  "./photo/eu___1.jpg",
+    "./photo/eu__2.jpg",
+    "./photo/eu__3.jpg",
+    "./photo/eu__4.jpg",
+    "./photo/eu__5.jpg",
+ ]);
 });
-
 
 document.querySelector(".btn-asia").addEventListener("click", () => {
-  showImages([
-    "./imaages/asia_1.jpg",
-    "./imaages/asia_2.jpg",
-    "./imaages/asia_3.jpg"
-  ]);
+showImages([
+  "./photo/asia__1.jpg",
+    "./photo/asia__2.jpg",
+    "./photo/asia__3.jpg",
+    "./photo/asia__4.jpg",
+    "./photo/asia__5.jpg",
+ ]);
 });
-
 
 document.querySelector(".btn-middleeast").addEventListener("click", () => {
-  showImages([
-    "./imaages/uae_1.jpg",
-    "./imaages/uae_2.jpg",
-    "./imaages/uae_4.jpg",
-  ]);
+showImages([
+  "./photo/uae__1.jpg",
+    "./photo/uae__2.jpg",
+    "./photo/uae__3.jpg",
+    "./photo/uae__4.jpg",
+    "./photo/uae__5.jpg",
+ ]);
 });
 
-// 戻るボタン
+
+
+
+
+
+
+// // 戻るボタン
 closeBtn.addEventListener("click", () => {
-  modal.style.display = "none";
-  gallery.innerHTML = "";
-});
+modal.style.display = "none";
+gallery.innerHTML = "";
+ });
 window.addEventListener("click", (e) => {
-  if (e.target === modal) {
-    modal.style.display = "none";
-    gallery.innerHTML = "";
-  }
+if (e.target === modal) {
+ modal.style.display = "none";
+gallery.innerHTML = "";
+}
 });
-
